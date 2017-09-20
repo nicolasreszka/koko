2c6aacac25029cd0ffadec1952f73aac3146cd4c
nicolasreszka <reszka@salle223-05.arda>
2017-09-20 12:22:18 +0200

    correction

--- tp1_threads/figure5.c
+++ tp1_threads/figure5.c
@@ -35,21 +35,21 @@ int main() {
 	int pid;
 
 	while(1) {
-	    pid = fork();
-	   
-	    /* fork() à échoué */
+		pid = fork();
+
+		/* fork() à échoué */
 		if (pid == -1) {
 			perror("Fork error");
 			exit(1);
 		} 
 	    
-	    if (pid > 0) {
-	    	printf("PID : %d\n", getpid());
+		if (pid > 0) {
+			printf("PID : %d\n", getpid());
 			break;
-	    }
+		}
 
-	    /* attendre 10sec pour te laisser une chance de tuer le processus */
-	  	sleep(10);
+		/* attendre 10sec pour te laisser une chance de tuer le processus */
+		sleep(10);
 	}
 
 	return EXIT_SUCCESS;
