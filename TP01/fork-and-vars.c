e8cc8a22e9d5c26846aef8a3827daf28a0ef9e5a
cau <cau@salle223-06.arda>
2017-09-20 10:53:44 +0200

    Ajout des commentaires

--- TP01/fork-and-vars.c
+++ TP01/fork-and-vars.c
@@ -9,9 +9,16 @@ void main(void)
 {
 	char i,buffver1;
 	int pid01,pid02; /* Ids des processus fils 1 et 2 */
-	int error;
+	int error; /* variable qui detecte les erreurs lors de open write et read */
 	int verificateur1,verificateur2,verificateur3;
-	verificateur1 = 10;
+	verificateur1 = 10; /* variable test */
+
+	/*
+	##############################################################################
+	#			Creation et initialisation d'un fichier à lire                   #
+	#                  en écrivant 0123456789 dedans                             #
+	##############################################################################
+	*/
 
 	verificateur2 = open("foo.dat",O_CREAT|O_TRUNC|O_WRONLY,0644);
 
@@ -25,8 +32,6 @@ void main(void)
 	{
 		error = write(verificateur2,&i,1);
 
-		printf("debug -> %c \n",i+'0');
-
 		if ( error < 0)
 		{
 			perror("Erreur lors de l'ecriture de foo.dat");
@@ -38,14 +43,36 @@ void main(void)
 
 	close(verificateur2); 
 
+	/*
+	###############################################################################
+	#				          Fin initialisation								  #
+	###############################################################################
+	*/
+
+
+	/*
+	###############################################################################
+	#                                Premier fork                                 #
+	#            But : Déterminer si les variables sonr hérité par le fils.       #
+	###############################################################################
+	*/
+
 	pid01 = fork();
 
-	if ( pid01 == -1)rror("erreur lors de l'ouverture de foo.dat n2.");
-			exit(-13);
+	/* ### Gestion de l'erreur possible du fork. ### */
+
+	if ( pid01 == -1)
 		{
 			perror("Erreur lors du fork n1");
 			exit(-1);
 		}
+
+
+	/*
+	################################################################################
+	#                         Code éxécuté par le fils.                            #
+	################################################################################
+	*/
 
 	else if ( pid01 == 0 )
 		{
@@ -63,27 +90,51 @@ void main(void)
 			exit(0);
 		}
 
-		verificateur3 = open("foo.dat",O_RDONLY,0444);
-		if ( verificateur3 == -1 )
+	/*
+	################################################################################
+	#                      Fin code éxécuté par le fils.                           #
+	################################################################################
+	*/
+
+
+	/*
+	################################################################################
+	#              Le pere va lire les 5 premiers numéro du fichier.               #
+	#                          Et le fils les 5 suivants                           #
+	# réponses attendues :                                                         #
+	#  (pere): 0                                                                   #
+	#  (pere): 1                                                                   #
+	#  (pere): 2                                                                   #
+	#  (pere): 3                                                                   #
+	#  (pere): 4                                                                   #
+	#  (fils): 5                                                                   #
+	#  (fils): 6                                                                   #
+	#  (fils): 7                                                                   #
+	#  (fils): 8                                                                   #
+	#  (fils): 9                                                                   #
+	################################################################################
+	*/
+
+	verificateur3 = open("foo.dat",O_RDONLY,0444);
+
+	if ( verificateur3 == -1 )
+	{
+		perror("erreur lors de l'ouverture de foo.dat n2.");
+		exit(-13);
+	}
+	for ( i=0 ; i<5 ; i++)
+	{
+		error = read(verificateur3,&buffver1,1);
+
+		if ( error < 0 )
 		{
-			perror("erreur lors de l'ouverture de foo.dat n2.");
-			exit(-13);
+			perror("Erreur lors de la lecture n1 de foo.dat.");
+			close(verificateur3);
+			exit(-14);
 		}
-		for ( i=0 ; i<5 ; i++)
-		{
-			error = read(verificateur3,&buffver1,1);
 
-			printf("debug2 -> %c \n",i+'0');
-
-			if ( error < 0 )
-			{
-				perror("Erreur lors de la lecture n1 de foo.dat.");
-				close(verificateur3);
-				exit(-14);
-			}
-
-			printf("(Pere) : %c \n",buffver1+'0');
-		}
+		printf("(Pere) : %c \n",buffver1+'0');
+	}
 
 
 	pid02 = fork();
@@ -96,12 +147,10 @@ void main(void)
 
 	else if ( pid02 == 0 )
 	{
-		for ( i=5 ; i< 10 ; i++ )
+		for ( i=5 ; i < 10 ; i++ )
 		{
 
 			error = read(verificateur3,&buffver1,1);
-
-			printf("debug3 -> %c \n",i='0');
 
 			if ( error < 0 )
 			{
@@ -111,9 +160,19 @@ void main(void)
 			}
 
 			printf("(Fils) : %c \n",buffver1+'0');
-			printf("Fin du processus fils n2\n");
-			exit(0);
 
 		}
+
+		if ( buffver1+'0' == '9' )
+		{
+			printf("Un fils herite des fd du père et des positions de ces fichiers. \n");
+		}
+		else
+		{
+			printf("Un fils n'herite pas des fd du père et des positions de ces fichiers. \n");
+		}
+
+		printf("Fin du processus fils n2\n");
+		exit(0);
 	}
 }
