76752037f24f764425a496a95c517c729c7e1fa6
cau <cau@salle223-12.arda>
2018-02-14 11:16:34 +0100

    ajout de bcp de fonctions

--- Fonctions/cryptmath.c
+++ Fonctions/cryptmath.c
@@ -318,7 +318,145 @@ void tea_decrypt_file(char * file_name,unsigned int key[4])
   close(fd);
 }
 
-void get_key_from_file(unsigned short size_key,char * file_name)
+unsigned char get_char_key_from_file(char * file_name);
 {
-  if ( size_key < )
+  int fd;
+  int err;
+  unsigned char result;
+  fd = open(file_name);
+  if ( fd == -1 )
+  {
+    perror("get_char_key_from_file : Erreur lors du open ");
+    exit(-1);
+  }
+  err = read(fd,&result,sizeof(char));
+  if ( err < sizeof(char))
+  {
+    perror("get_char_key_from_file : Erreur lors du read ");
+    exit(-1);
+  }
+  err = close(fd);
+  if ( err == -1)
+  {
+    perror("get_char_key_from_file : Erreur lors du close ");
+    exit(-1);
+  }
+  return result;
+}
+
+unsigned short get_short_key_from_file(char * file_name);
+{
+  int fd;
+  int err;
+  unsigned short result;
+  fd = open(file_name);
+  if ( fd == -1 )
+  {
+    perror("get_short_key_from_file : Erreur lors du open ");
+    exit(-1);
+  }
+  err = read(fd,&result,sizeof(short));
+  if ( err < sizeof(short))
+  {
+    perror("get_short_key_from_file : Erreur lors du read ");
+    exit(-1);
+  }
+  err = close(fd);
+  if ( err == -1)
+  {
+    perror("get_short_key_from_file : Erreur lors du close ");
+    exit(-1);
+  }
+  return result;
+}
+
+unsigned int get_int_key_from_file(char * file_name);
+{
+  int fd;
+  int err;
+  unsigned int result;
+  fd = open(file_name);
+  if ( fd == -1 )
+  {
+    perror("get_int_key_from_file : Erreur lors du open ");
+    exit(-1);
+  }
+  err = read(fd,&result,sizeof(int));
+  if ( err < sizeof(int))
+  {
+    perror("get_int_key_from_file : Erreur lors du read ");
+    exit(-1);
+  }
+  err = close(fd);
+  if ( err == -1)
+  {
+    perror("get_int_key_from_file : Erreur lors du close ");
+    exit(-1);
+  }
+  return result;
+}
+
+unsigned long get_long_key_from_file(char * file_name);
+{
+  int fd;
+  int err;
+  unsigned long result;
+  fd = open(file_name);
+  if ( fd == -1 )
+  {
+    perror("get_long_key_from_file : Erreur lors du open ");
+    exit(-1);
+  }
+  err = read(fd,&result,sizeof(long));
+  if ( err < sizeof(long))
+  {
+    perror("get_long_key_from_file : Erreur lors du read ");
+    exit(-1);
+  }
+  err = close(fd);
+  if ( err == -1)
+  {
+    perror("get_long_key_from_file : Erreur lors du close ");
+    exit(-1);
+  }
+  return result;
+}
+
+unsigned long long get_long_long_key_from_file(char * file_name);
+{
+  int fd;
+  int err;
+  unsigned long long result;
+  fd = open(file_name);
+  if ( fd == -1 )
+  {
+    perror("get_long_long_key_from_file : Erreur lors du open ");
+    exit(-1);
+  }
+  err = read(fd,&result,sizeof(long long));
+  if ( err < sizeof(long long))
+  {
+    perror("get_long_long_key_from_file : Erreur lors du read ");
+    exit(-1);
+  }
+  err = close(fd);
+  if ( err == -1)
+  {
+    perror("get_long_long_key_from_file : Erreur lors du close ");
+    exit(-1);
+  }
+  return result;
+}
+
+void * get_unkown_key_from_file(char * file_name)
+{
+  int fd;
+  int err;
+  void * result;
+  fd = open(file_name);
+  if ( fd == -1 )
+  {
+    perror("get_unkown_key_from_file : Erreur lors du open ");
+    exit(-1);
+  }
 }
