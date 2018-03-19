396bb32462d24c0bebb1376481c9f4a9093a5f07
nicolasreszka <reszka@salle234-11.arda>
2018-03-19 16:18:50 +0000

    property

--- tp20_rsa/rsa.c
+++ tp20_rsa/rsa.c
@@ -5,7 +5,7 @@
 /* La méthode RSA consiste à :
 ** - Choisir deux nombres premiers p et q "assez grands" 
 ** - Calculer le modulus n = p * q
-** - Calculer phi = euler(m)
+** - Calculer phi = euler(n)
 ** - Choisir e < phi tel que e et phi premiers entre eux 
 ** - Choisir d < phi tel que (e * d) % phi == 1
 ** - Le couple (n, e) est la clef publique, le cypher du "plain text" est : (b**e) mod n
@@ -14,13 +14,13 @@
 
 
 /* Il faut donc coder : 
-** - alogrithme d'euclide 
-** - théorème de bézout 
-** - fonction d'euler
+** - alogrithme d'euclide(a,b) -> renvoie le plus grand diviseur commun de a et b
+** - théorème de bézout(a,b) -> renvoie les coefficients de bézout ( ax + by = pgcd(a,b) )
+** - fonction d'euler(n) -> phi(n) =  (1 - n ** 1) * (1 - n ** 2) ... * (1 - n ** k)
+**   finalement on n'a pas besoin de euler grâce à la propiété suivante : phi(p*q) = (p-1) * (q-1) 
 ** - generate_public_key(p,q)
 ** - generate_private_key(k, p,q) avec k = e ou d
 */
-
 
 /* Plus grand diviseur commun de a et b */
 unsigned long int	gcd(unsigned long int a, unsigned long int b)
